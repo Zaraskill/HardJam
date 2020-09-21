4b4c92a5df4901ea7c19c108aa7fe50e1927c75c
Corentin <corentin.lelong77@gmail.com>
2020-09-21 11:45:08 +0200

    Proto Change color

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public float timerSwitch = 3;
+    private float timeLeft;
+    public List<Color> colorList;
+    [HideInInspector] public ColorChange[] objectChange;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeLeft = timerSwitch;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (UpdateTimer())
+        {
+            SwitchColor();
+        }
+    }
+
+
+    private bool UpdateTimer()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            timeLeft = timerSwitch;
+            return true;
+        }
+        return false;
+    }
+
+    private void SwitchColor()
+    {
+        objectChange = FindObjectsOfType<ColorChange>();
+        //changeColor.ChangeColorSaleConnard();
     }
 }
