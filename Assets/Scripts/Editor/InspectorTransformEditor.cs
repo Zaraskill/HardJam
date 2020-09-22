dad18499d2da51e3e54a3b5347af9d5b77018305
Nicolas ROGE <nicolas.roge.contact@gmail.com>
2020-09-22 15:08:46 +0200

    Player + Multiplayer | Add cursor with moving system and click from joystick on Player

---
+++ Assets/Scripts/Editor/InspectorTransformEditor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Transform))]
+[CanEditMultipleObjects]
+public class InspectorTransformEditor : Editor
+{
+    //Unity's built-in editor
+    Editor _defaultEditor;
+    Transform _transform;
+
+    void OnEnable()
+    {
+        _defaultEditor = Editor.CreateEditor(targets, Type.GetType("UnityEditor.TransformInspector, UnityEditor"));
+        _transform = target as Transform;
+    }
+
+    void OnDisable()
+    {
+        MethodInfo disableMethod = _defaultEditor.GetType().GetMethod("OnDisable", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        if (disableMethod != null)
+            disableMethod.Invoke(_defaultEditor, null);
+        DestroyImmediate(_defaultEditor);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.LabelField("Local Space", EditorStyles.boldLabel);
+        _defaultEditor.OnInspectorGUI();
+
+        //Show World Space Transform
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("World Space", EditorStyles.boldLabel);
+
+        GUI.enabled = false;
+        Vector3 localPosition = _transform.localPosition;
+        _transform.localPosition = _transform.position;
+
+        Quaternion localRotation = _transform.localRotation;
+        _transform.localRotation = _transform.rotation;
+
+        Vector3 localScale = _transform.localScale;
+        _transform.localScale = _transform.lossyScale;
+
+        _defaultEditor.OnInspectorGUI();
+        _transform.localPosition = localPosition;
+        _transform.localRotation = localRotation;
+        _transform.localScale = localScale;
+        GUI.enabled = true;
+    }
+
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,109 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Rewired;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Rewired Player Settings")]
+    public int _PlayerId;
+    private Rewired.Player inputPlayer;
+
+    [Header("Cursor Object")]
+    public Image cursor;
+    private RaycastHit hit;
+    public float raduisSphereCast = 0.5f;
+    [Space(5)]
+    public float leftMax;
+    public float rightMax;
+    public float UpMax;
+    public float DownMax;
+
+    [Header("Player Settings")]
+    public LayerMask layerMask;
+    public float moveSpeedValue;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        inputPlayer = ReInput.players.GetPlayer(_PlayerId);
+
+        hit = new RaycastHit();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inputPlayer.GetButtonDown("Select"))
+        {
+            CursorRaycast();
+        }
+
+        CursorMovement();
     }
+
+    private void CursorMovement()
+    {
+
+        float horizontal = 0;
+        horizontal = inputPlayer.GetAxis("HorizontalMove");
+        float vertical = 0;
+        vertical = inputPlayer.GetAxis("VerticalMove");
+
+        Vector2 actualPosition = cursor.rectTransform.anchoredPosition;
+
+        actualPosition.x += horizontal * moveSpeedValue * Time.deltaTime * 100;
+        actualPosition.y += vertical * moveSpeedValue * Time.deltaTime * 100;
+
+        if (actualPosition.x < leftMax)
+        {
+            actualPosition.x = leftMax;
+        }
+        else if (actualPosition.x > rightMax)
+        {
+            actualPosition.x = rightMax;
+        }
+
+        if (actualPosition.y < DownMax)
+        {
+            actualPosition.y = DownMax;
+        }
+        else if (actualPosition.y > UpMax)
+        {
+            actualPosition.y = UpMax;
+        }
+
+        cursor.rectTransform.anchoredPosition = actualPosition;
+    }
+
+    private void CursorRaycast()
+    {
+
+        if (Physics.SphereCast(cursor.transform.position, raduisSphereCast, cursor.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+//#if UNITY_EDITOR
+//            Debug.DrawRay(cursor.transform.position, cursor.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+//            Debug.Log("Did Hit : " + hit.collider.gameObject.name + "" + _PlayerId);
+//#endif
+        }
+        else
+        {
+
+//#if UNITY_EDITOR
+//            Debug.DrawRay(cursor.transform.position, cursor.transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+//            Debug.Log("Did not Hit " + _PlayerId);
+//#endif
+        }
+    }
+
+#if UNITY_EDITOR
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        if(hit.transform != null)
+            Gizmos.DrawSphere(hit.transform.position, raduisSphereCast);
+    }
+#endif
 }
