c46ad62162bff9ba492a0d00ac8d0cc9f2eb0f9a
Nicolas ROGE <nicolas.roge.contact@gmail.com>
2020-09-23 11:21:00 +0200

    Set volume with slider on Menu

---
+++ Assets/Scripts/SettingSound/Editor/EditorSetVolume.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+namespace PackageCreator.Sounds
+{
+    [CustomEditor(typeof(SetVolume))]
+    public class EditorSetVolume : Editor
+    {
+        SerializedProperty mixer;
+        SerializedProperty slider;
+        SerializedProperty exposedMixerParameter;
+        SerializedProperty textVolume;
+        float _sliderDataSave;
+
+        private void OnEnable()
+        {
+            mixer = serializedObject.FindProperty("mixer");
+            slider = serializedObject.FindProperty("slider");
+            exposedMixerParameter = serializedObject.FindProperty("_nameExposedParameter");
+            textVolume = serializedObject.FindProperty("textObject");
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+
+        public override void OnInspectorGUI()
+        {
+            SetVolume script = (SetVolume)target;
+
+            EditorGUILayout.LabelField("Settings Volume Slider", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(mixer, new GUIContent("ref Audio Mixer"));
+            EditorGUILayout.Space(5);
+            EditorGUILayout.PropertyField(slider, new GUIContent("ref UI Slider"));
+            EditorGUILayout.Space(5);
+            EditorGUILayout.PropertyField(exposedMixerParameter, new GUIContent("Name ExposedMixer"));
+            EditorGUILayout.Space(10);
+            EditorGUILayout.LabelField("Actual Saved State Volume", EditorStyles.boldLabel);
+            GUI.enabled = false;
+            _sliderDataSave = EditorGUILayout.Slider(PlayerPrefs.GetFloat(script._nameExposedParameter, 1f), 0f, 1f);
+            GUI.enabled = true;
+            EditorGUILayout.Space(5);
+            EditorGUILayout.LabelField("Settings Text Volume", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(textVolume, new GUIContent("ref Percent Text"));
+
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Scripts/SettingSound/SetVolume.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace PackageCreator.Sounds
+{
+    public class SetVolume : MonoBehaviour //Script audiomixer for sound with a slider option
+    {
+        [HideInInspector]
+        public AudioMixer mixer; //Don't forget to make ExposedParameter on AudioMixer by right click on audiomixer group on Volume and expose it !
+        [HideInInspector]
+        public Slider slider; //Don't forget to make min value at 0.001 and set function on slider Value Changed
+        [HideInInspector]
+        public Text textObject;
+        [HideInInspector]
+        public string _nameExposedParameter;
+
+        void Start()
+        {
+            Init();
+        }
+
+        //Function Init to be used by other script for set value of sound 
+        public void Init()
+        {
+            //value save data
+            slider.value = PlayerPrefs.GetFloat(_nameExposedParameter, 1f);
+            UpdateTextObject();
+        }
+
+        //Function to change audiomixer volume with value changed of slider
+        public void SetLevel()
+        {
+            float sliderValue = slider.value;
+            //Mixer float form slider value 
+            mixer.SetFloat(_nameExposedParameter, Mathf.Log10(sliderValue) * 20);
+            //Save Data
+            PlayerPrefs.SetFloat(_nameExposedParameter, sliderValue);
+            UpdateTextObject();
+        }
+
+        //Change text for percent
+        public void UpdateTextObject()
+        {
+            //percent of slider value
+            textObject.text = Mathf.Round(slider.value * 100) + "%";
+        }
+
+        // ------------------------------------------------------------------------------------------------------------------------------------------- //
+    }
+}
