0be71d8d181535668725838db1152991609a6033
Corentin <corentin.lelong77@gmail.com>
2020-09-23 18:45:06 +0200

    Menu options with dropdown color blindness

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,20 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickPlay()
     {
-        
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnClickOptions(bool sideChange)
+    {
+        if (sideChange)
+        {
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(true);
+        }
+        else
+        {
+            mainMenu.SetActive(true);
+            optionsMenu.SetActive(false);
+        }
+    }
+
+    public void Toggle
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
     }
 }
