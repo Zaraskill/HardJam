439fc891b5c46b55d29f08090075102d691182cd
Falouski <falou91@gmail.com>
2020-09-24 10:41:39 +0200

    Marcus scène Music

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void PlayNo()
+    {
+        AkSoundEngine.PostEvent("Play_AllNo", gameObject);
+    }
+
+    public void PlayYes()
+    {
+        AkSoundEngine.PostEvent("Play_AllYes", gameObject);
+    }
+
+    public void PlayNext()
+    {
+        AkSoundEngine.PostEvent("Play_AllNext", gameObject);
+    }
+
+    public void Play30sec()
+    {
+        AkSoundEngine.PostEvent("Play_All30sec", gameObject);
+    }
+}
