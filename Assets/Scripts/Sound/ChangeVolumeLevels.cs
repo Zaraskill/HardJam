c3967ece2cd6f2582c291cd962af7c83df522389
Falouski <falou91@gmail.com>
2020-09-25 09:58:05 +0200

    ChangeVolume & Pause

---
+++ Assets/Scripts/Sound/ChangeVolumeLevels.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeVolumeLevels : MonoBehaviour
+{
+
+    public Slider thisSlider;
+    public float masterVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSpecificVolume(string whatValue)
+    {
+
+        float sliderValue = thisSlider.value;
+
+        if (whatValue == "Master")
+        {
+            masterVolume = thisSlider.value;
+            AkSoundEngine.SetRTPCValue("MasterVolume", masterVolume)
+        }
+
+    }   
+
+
+}
